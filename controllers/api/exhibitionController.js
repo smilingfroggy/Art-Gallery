41b2132e732ba45a037d3602e411561d33c21370
smilingfroggy <smilingfroggy@hotmail.com>
2022-04-29 23:57:53 +0800

    feat: add /exhibitions/:id/artists API

--- controllers/api/exhibitionController.js
+++ controllers/api/exhibitionController.js
@@ -4,6 +4,7 @@ const Exhibition = db.Exhibition
 const ExhibitionImage = db.ExhibitionImage
 const Medium = db.Medium
 const Artist = db.Artist
+const ArtistImage = db.ArtistImage
 
 const exhibitionController = {
   getExhibitions: async (req, res) => {
@@ -30,7 +31,7 @@ const exhibitionController = {
       console.log(error)
     }
   },
-  getExhibition: async (req, res) => {
+  getExhibitionArtwork: async (req, res) => {
     try {
       const exhibition_rawData = await Exhibition.findByPk(req.params.exhibitionId, {
         include: [
@@ -54,7 +55,45 @@ const exhibitionController = {
       console.log(error)
     }
 
-  }
+  },
+  getExhibitionArtists: async (req, res) => {
+    try {
+      const exhibitionArtists_rawData = await Exhibition.findByPk(req.params.exhibitionId, {
+        where: { 'privacy': 2 },
+        attributes: ['id', 'name'],
+        include: [
+          {
+            model: Artwork, as: 'ContainedArtworks',
+            attributes: ['id'],
+            through: { attributes: [] },
+            include: {
+              model: Artist, as: 'Creators', through: { attributes: [] },
+              attributes: ['id', 'name', 'birthYear', 'deathYear', 'introduction'],
+              include: { model: ArtistImage, attributes: { exclude: ['createdAt', 'updatedAt'] } }
+            }
+          },
+        ]
+      })
+
+      if (!exhibitionArtists_rawData) {
+        return res.json({ status: 'failure', message: 'No artists found!'})
+      }
+      // 去除重複的創作者；每件作品創作者可能不同、每件作品可有多個創作者
+      const creators = []
+      exhibitionArtists_rawData.toJSON().ContainedArtworks.forEach(work => {
+        work.Creators.forEach(creator => {
+          if (!creators.find(creator_added => creator_added.id === creator.id)) {
+            creators.push(creator)
+          }
+        })
+      })
+
+      return res.json(creators)
+
+    } catch (error) {
+      console.log(error)
+    }
+  },
 }
 
 module.exports = exhibitionController
--- routes/apis.js
+++ routes/apis.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const exhibitionController = require('../controllers/api/exhibitionController')
 
 router.get('/exhibitions/', exhibitionController.getExhibitions)
-router.get('/exhibitions/:exhibitionId', exhibitionController.getExhibition)
+router.get('/exhibitions/:exhibitionId', exhibitionController.getExhibitionArtwork)
+router.get('/exhibitions/:exhibitionId/artists', exhibitionController.getExhibitionArtists)
 
 module.exports = router;
