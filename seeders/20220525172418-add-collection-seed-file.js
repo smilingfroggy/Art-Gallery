31c68034d0d52ce921fce012e013ad2f5e855ea0
smilingfroggy <smilingfroggy@hotmail.com>
2022-05-28 01:17:44 +0800

    modify collection artworks seed data

--- seeders/20220525172418-add-collection-seed-file.js
+++ seeders/20220525172418-add-collection-seed-file.js
@@ -5,18 +5,13 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     const users = await queryInterface.sequelize.query('SELECT id FROM Users WHERE isAdmin = 0', { type: queryInterface.sequelize.QueryTypes.SELECT})
     // [ {id: 1}, {id: 2} ... ]
+    const names = [['Connoisseur', 'The Persistence of Memory'], ['Pulse of the Earth', 'Inspirations'], ['Free spirited', 'Delicate touches']]
 
-    await queryInterface.bulkInsert('Collections', users.map(user => { return [{
+    await queryInterface.bulkInsert('Collections', users.map((user, index) => { 
+      console.log('user index', index)
+      return [{
       UserId: user.id,
       name: 'Favorite List',
-      description: faker.lorem.sentence(),
-      privacy: 2,
-      url: '',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      UserId: user.id,
-      name: user.id % 2 ? 'Renting List' : 'Connoisseur',
       description: faker.lorem.sentence(),
       privacy: 0,
       url: '',
@@ -24,7 +19,15 @@ module.exports = {
       updatedAt: new Date()
     },{
       UserId: user.id,
-      name: user.id % 2 ? 'The Persistence of Memory' : 'Inspirations',
+      name: names[index][0],
+      description: faker.lorem.sentence(),
+      privacy: 2,
+      url: '',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      UserId: user.id,
+      name: names[index][1],
       description: faker.lorem.sentence(),
       privacy: 2,
       url: '',
@@ -35,6 +38,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Collection', null, {})
+    await queryInterface.bulkDelete('Collections', null, {})
   }
 };
