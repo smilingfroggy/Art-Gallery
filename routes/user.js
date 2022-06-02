e3b823e9ef454645d77a9ddee46874b42a6114a2
smilingfroggy <smilingfroggy@hotmail.com>
2022-06-03 03:01:10 +0800

    fix: login failure redirect

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
-const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 
 router.get('/signup', (req, res) => res.render('signup'))
 router.post('/signup', userController.signUp)
 router.get('/login', (req, res) => res.render('login'))
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }) , userController.login)
+router.post('/login', passport.authenticate('local', { failureRedirect: '/user/login', failureMessage: true }) , userController.login)
 router.post('/logout', userController.logout)
 router.use('/', generalErrorHandler)
 
