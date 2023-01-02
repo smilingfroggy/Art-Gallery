ee56ae05ea0004ac7227f0ea606d1d5ec728e7e4
smilingfroggy <smilingfroggy@hotmail.com>
2023-01-03 01:20:23 +0800

    refactor: userContoller.signUp

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,33 +2,12 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User, Collection } = db
 const helpers = require('../helpers/auth-helpers')
+const userService = require('../services/userService')
 
 const userController = {
   signUp: async (req, res, next) => {
     try {
-      const { name, password, passwordCheck, email } = req.body
-      if (!name || !password || !passwordCheck || !email) throw new Error('Please provide complete messages')
-      if (password !== passwordCheck) throw new Error('Passwords do not match')
-
-      // check if email has been registered
-      const userRecord = await User.findOne({
-        where: { email }
-      })
-      if (userRecord) throw new Error('Email has been registered')
-
-      // create new user and default collection
-      const newUser = await User.create({
-        name, 
-        email,
-        password: bcrypt.hashSync(password, 10),
-      })
-
-      await Collection.create({
-        UserId: newUser.id,
-        name: 'Favorite List',
-        privacy: 0
-      })
-
+      await userService.signUp(req)
       req.flash('success_messages', 'Registered Successfully')
       return res.redirect('/user/login')
     } catch (error) {
