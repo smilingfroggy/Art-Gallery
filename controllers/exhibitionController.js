fcef98d36cc71b4047d844737437db356266f760
smilingfroggy <smilingfroggy@hotmail.com>
2023-01-05 23:36:15 +0800

    refactor: exhibition controller, add exhibition service

--- controllers/exhibitionController.js
+++ controllers/exhibitionController.js
@@ -1,35 +1,9 @@
-const db = require('../models')
-const { Artwork, Artist, ArtistImage, ArtworkImage, Exhibition, ExhibitionImage, Medium } = db
-const helpers = require('../helpers/auth-helpers')
-const IMAGE_NOT_AVAILABLE = 'https://i.imgur.com/nVNO3Kj.png'
-const ARTIST_AVATAR_NOT_AVAILABLE = 'https://i.imgur.com/QJrNwMz.jpg'
+const exhibitionService = require('../services/exhibitionService')
 
 const exhibitionController = {
   getExhibitions: async (req, res, next) => {
     try {
-      const exhibitions_rawData = await Exhibition.findAll({
-        where: { 'privacy': 2 },
-        include: [
-          { model: ExhibitionImage, attributes: ['url', 'type', 'description'] },
-          { model: Artwork, as: 'ContainedArtworks', attributes: ['id'], through: { attributes: [] } },
-        ],
-        attributes: { exclude: ['createdAt', 'updatedAt'] },
-        order: ['date_start']
-      })
-
-      let exhibitionsData = exhibitions_rawData.map(exhibition => {
-        let count = exhibition.ContainedArtworks.length
-        delete exhibition.dataValues.ContainedArtworks
-        let useImage = exhibition.dataValues.ExhibitionImages.find(images => images.type === 'poster')?.url || IMAGE_NOT_AVAILABLE
-        return {
-          ...exhibition.dataValues,
-          date_start: exhibition.dataValues.date_start?.toISOString().slice(0, 10),
-          date_end: exhibition.dataValues.date_end?.toISOString().slice(0, 10),
-          introduction: exhibition.dataValues.introduction?.slice(0, 55) + "...",
-          ExhibitionImages: useImage,
-          artwork_sum: count
-        }
-      })
+      let exhibitionsData = await exhibitionService.getExhibitions(req, res)
       return res.render('exhibitions', { exhibitionsData })
     } catch (error) {
       console.log(error)
@@ -38,34 +12,7 @@ const exhibitionController = {
   },
   getExhibitionInfo: async (req, res, next) => {
     try {
-      const exhibition_rawData = await Exhibition.findOne({
-        where: {
-          privacy: 2,
-          id: req.params.exhibitionId
-        },
-        include: [
-          { model: ExhibitionImage, attributes: ['url', 'type', 'description'] },
-          {
-            model: Artwork, as: 'ContainedArtworks',
-            attributes: ['id', 'artistName', 'name'],
-            through: { attributes: [] }
-          },
-        ],
-        attributes: { exclude: ['createdAt', 'updatedAt'] },
-      })
-
-      if (!exhibition_rawData) throw new Error('Exhibition unavailable')
-
-      let result = exhibition_rawData.toJSON()
-      result.artwork_sum = result.ContainedArtworks.length
-      result.date_start = result.date_start?.toISOString().slice(0, 10)
-      result.date_end = result.date_end?.toISOString().slice(0, 10)
-
-      // console.log(result.ContainedArtworks) 
-      let usePoster = result.ExhibitionImages.find(images => images.type === 'poster')?.url || IMAGE_NOT_AVAILABLE
-      result.poster = usePoster
-
-      // return res.json(result)
+      const result = await exhibitionService.getExhibition(req, res)
       return res.render('exhibition_info', { exhibition: result })
     } catch (error) {
       console.log(error)
@@ -74,76 +21,9 @@ const exhibitionController = {
   },
   getExhibitionArtworks: async (req, res, next) => {
     try {
-      const user = helpers.getUser(req) || null
-      let addedArtworks = user?.addedArtworks || new Set()
-      let favoriteArtworks = user?.favoriteArtworks || []
-
-      const exhibitionArtworks_rawData = await Exhibition.findOne({
-        where: {
-          privacy: 2,
-          id: req.params.exhibitionId
-        },
-        attributes: ['id', 'name', 'privacy'],
-        include: {
-          model: Artwork, as: 'ContainedArtworks',
-          attributes: ['id', 'artistName', 'name', 'creationTime', 'height', 'width', 'depth'],
-          through: { attributes: [] },
-          include: [
-            { model: Medium, attributes: ['name'] },
-            { model: ArtworkImage, attributes: ['url'] },
-            {
-              model: Artist, as: 'Creators', through: { attributes: [] },
-              attributes: ['id', 'name', 'birthYear', 'deathYear', 'introduction'],
-              include: { model: ArtistImage, attributes: { exclude: ['createdAt', 'updatedAt'] } }
-            }
-          ]
-        },
-      })
-
-      if (!exhibitionArtworks_rawData) throw new Error('Exhibition artwork unavailable')
-
-      let result = exhibitionArtworks_rawData.toJSON()
-      result.artwork_sum = result.ContainedArtworks.length
-      // 整理藝術品資料: name, medium, time, image, size...
-      result.ContainedArtworks.forEach(work => {
-        work.name = work.name?.slice(0, 20)
-        work.medium = work.Medium.name
-        work.creationTime = work.creationTime ? new Date(work.creationTime).getFullYear() : ""
-        work.image = work.ArtworkImages[0]?.url || IMAGE_NOT_AVAILABLE
-        delete work.Medium
-        delete work.ArtworkImages
-        work.size = work.depth ? (work.height + "x" + work.width + "x" + work.depth) : (work.height + "x" + work.width)
-        work.isAdded = addedArtworks.has(work.id)
-        work.isFavorite = favoriteArtworks.includes(work.id)
-      })
-
-      // 找出不重複的創作者；每件作品創作者可能不同、每件作品可有多個創作者
-      const creators = []
-      result.ContainedArtworks.forEach(work => {
-        work.Creators.forEach(creator => {
-          if (!creators.find(creator_added => creator_added.id === creator.id)) {
-            creators.push(creator)
-          }
-        })
-        delete work.Creators
-      })
-
-      // 藝術家照片：優先顯示創作者大頭照(type: head)
-      creators.forEach(creator => {
-        if (creator.ArtistImages.length === 0) {
-          creator.ArtistImages = ARTIST_AVATAR_NOT_AVAILABLE  //預設空白大頭照
-        } else {
-          const headImg = creator.ArtistImages.find(image => image.type === 'head')
-          if (headImg) {  // imgur url + b => big thumbnail 
-            creator.ArtistImages = headImg.url.split('.jpg')[0] + 'b.jpg'
-          } else {
-            creator.ArtistImages = creator.ArtistImages[0].url.split('.jpg')[0] + 'b.jpg'
-          }
-        }
-        creator.introduction = creator.introduction?.split("。")[0] + "。"
-      })
-
-      result.Creators = creators
+      const result = await exhibitionService.getExhibitionArtworks(req, res)
+      const Creators = await exhibitionService.getExhibitionArtists(req, res)
+      result.Creators = Creators
 
       return res.render('exhibition_artworks', { exhibition: result })
     } catch (error) {
@@ -153,19 +33,8 @@ const exhibitionController = {
   },
   getExhibitionImages: async (req, res, next) => {
     try {
-      const exhibitionImages_rawData = await Exhibition.findOne({
-        where: {
-          privacy: 2,
-          id: req.params.exhibitionId
-        },
-        attributes: ['id', 'name'],
-        include: {
-          model: ExhibitionImage, attributes: ['id', 'url', 'type', 'description'],
-        },
-      })
-      if (!exhibitionImages_rawData) throw new Error('Exhibition images unavailable')
-      const exhibitionImages = exhibitionImages_rawData.toJSON()
-
+      const exhibitionImages = await exhibitionService.getExhibitionImages(req, res)
+    
       return res.render('exhibition_images', { exhibition: exhibitionImages })
     } catch (error) {
       console.log(error)
---
+++ services/exhibitionService.js
@@ -0,0 +1,186 @@
+const db = require('../models');
+const { Artwork, ArtworkImage, Exhibition, ExhibitionImage, Medium, Artist, ArtistImage } = db
+const sequelize = require('sequelize')
+const helpers = require('../helpers/auth-helpers')
+const IMAGE_NOT_AVAILABLE = 'https://i.imgur.com/nVNO3Kj.png'
+const ARTIST_AVATAR_NOT_AVAILABLE = 'https://i.imgur.com/QJrNwMz.jpg'
+
+const createError = require('http-errors');
+
+
+const exhibitionService = {
+  getExhibitions: async (req, res) => {  // cb
+    const exhibitions_rawData = await Exhibition.findAll({
+      where: { 'privacy': 2 },
+      include: [
+        { model: ExhibitionImage, attributes: ['url', 'type', 'description'] },
+      ],
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+        include: [[sequelize.literal(`(
+          SELECT COUNT (*) FROM exhibitionArtworks As ca WHERE ca.exhibitionId = exhibition.id
+        )`), 'artwork_sum']]
+      },
+      order: ['date_start']
+    })
+
+    let exhibitionsData = exhibitions_rawData.map(exhibition => {
+      let useImage = exhibition.dataValues.ExhibitionImages.find(images => images.type === 'poster')?.url || IMAGE_NOT_AVAILABLE
+      return {
+        ...exhibition.dataValues,
+        date_start: exhibition.dataValues.date_start?.toISOString().slice(0, 10),
+        date_end: exhibition.dataValues.date_end?.toISOString().slice(0, 10),
+        introduction: exhibition.dataValues.introduction?.slice(0, 55) + "...",
+        ExhibitionImages: useImage,
+      }
+    })
+
+    // return cb(exhibitionsData)
+    return exhibitionsData
+  },
+  getExhibition: async (req, res) => {
+    // selected exhibition's info, sum of works, poster
+    const exhibition_rawData = await Exhibition.findOne({
+      where: {
+        privacy: 2,
+        id: req.params.exhibitionId
+      },
+      include: [
+        { model: ExhibitionImage, attributes: ['url', 'type', 'description'] },
+        {
+          model: Artwork, as: 'ContainedArtworks',
+          attributes: ['id', 'artistName', 'name'],
+          through: { attributes: [] }
+        },
+      ],
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+    })
+
+    if (!exhibition_rawData) {
+      res.status(404)
+      throw new Error('Exhibition unavailable')
+    }
+
+    let result = exhibition_rawData.toJSON()
+    result.artwork_sum = result.ContainedArtworks.length
+    result.date_start = result.date_start?.toISOString().slice(0, 10)
+    result.date_end = result.date_end?.toISOString().slice(0, 10)
+    result.poster = result.ExhibitionImages.find(images => images.type === 'poster')?.url || IMAGE_NOT_AVAILABLE
+    return result
+  },
+  getExhibitionArtworks: async (req, res) => {
+    // selected exhibition's artworks details & added to user's collections
+    const user = helpers.getUser(req) || null
+    let addedArtworks = user?.addedArtworks || new Set()
+    let favoriteArtworks = user?.favoriteArtworks || []
+
+    const exhibitionArtworks_rawData = await Exhibition.findOne({
+      where: {
+        privacy: 2,
+        id: req.params.exhibitionId
+      },
+      attributes: ['id', 'name', 'privacy'],
+      include: {
+        model: Artwork, as: 'ContainedArtworks',
+        attributes: ['id', 'artistName', 'name', 'creationTime', 'height', 'width', 'depth'],
+        through: { attributes: [] },
+        include: [
+          { model: Medium, attributes: ['name'] },
+          { model: ArtworkImage, attributes: ['url'] },
+          {
+            model: Artist, as: 'Creators', through: { attributes: [] },
+            attributes: ['id', 'name', 'birthYear', 'deathYear', 'introduction'],
+          }
+        ]
+      },
+    })
+    if (!exhibitionArtworks_rawData) {
+      res.status(404)
+      throw new Error('Exhibition artwork unavailable')
+    }
+
+    let result = exhibitionArtworks_rawData.toJSON()
+    result.artwork_sum = result.ContainedArtworks.length
+    // 整理藝術品資料: name, medium, time, image, size...
+    result.ContainedArtworks.forEach(work => {
+      work.name = work.name?.slice(0, 20)
+      work.medium = work.Medium.name
+      work.creationTime = work.creationTime ? new Date(work.creationTime).getFullYear() : ""
+      work.image = work.ArtworkImages[0]?.url || IMAGE_NOT_AVAILABLE
+      delete work.Medium
+      delete work.ArtworkImages
+      work.size = work.depth ? (work.height + "x" + work.width + "x" + work.depth) : (work.height + "x" + work.width)
+      work.isAdded = addedArtworks.has(work.id)
+      work.isFavorite = favoriteArtworks.includes(work.id)
+    })
+
+    return result
+  },
+  getExhibitionArtists: async (req, res) => {
+    const exhibitionArtists_rawData = await Exhibition.findOne({
+      where: {
+        privacy: 2,
+        id: req.params.exhibitionId
+      },
+      attributes: ['id', 'name'],
+      include: {
+        model: Artwork, as: 'ContainedArtworks',
+        attributes: ['id'],
+        through: { attributes: [] },
+        include: {
+          model: Artist, as: 'Creators', through: { attributes: [] },
+          attributes: ['id', 'name', 'introduction'],
+          order: [['birthYear', 'DESC']],
+          include: { model: ArtistImage, attributes: { exclude: ['createdAt', 'updatedAt'] } }
+        }
+      }
+    })
+
+    if (!exhibitionArtists_rawData) {
+      res.status(404)
+      throw new Error('No artists found!')
+    }
+    // 去除重複的創作者；每件作品創作者可能不同、每件作品可有多個創作者
+    const creators = new Map()
+    exhibitionArtists_rawData.toJSON().ContainedArtworks.forEach(work => {
+      work.Creators.forEach(creator => {
+        if (!creators.get(creator.id)) {
+          // 藝術家照片：優先顯示創作者大頭照(type: head)
+          if (creator.ArtistImages.length === 0) {
+            creator.ArtistImages = ARTIST_AVATAR_NOT_AVAILABLE  //預設空白大頭照
+          } else {
+            const headImg = creator.ArtistImages.find(image => image.type === 'head')
+            if (headImg) {  // imgur url + b => big square 
+              creator.ArtistImages = headImg.url.split('.jpg')[0] + 'b.jpg'
+            } else {
+              creator.ArtistImages = creator.ArtistImages[0].url.split('.jpg')[0] + 'b.jpg'
+            }
+          }
+          creator.introduction = creator.introduction?.split("。")[0] + "。"
+          creators.set(creator.id, creator)
+        }
+      })
+    })
+    return Array.from(creators.values())
+
+  },
+  getExhibitionImages: async (req, res, next) => {
+    const exhibitionImages_rawData = await Exhibition.findOne({
+      where: {
+        privacy: 2,
+        id: req.params.exhibitionId
+      },
+      attributes: ['id', 'name'],
+      include: {
+        model: ExhibitionImage, attributes: ['id', 'url', 'type', 'description'],
+      }
+    })
+    if (!exhibitionImages_rawData) {
+      res.status(404)
+      throw new Error('Exhibition images unavailable')
+    }
+    return exhibitionImages_rawData.toJSON()
+  }
+}
+
+module.exports = exhibitionService
