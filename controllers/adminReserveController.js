ea985dc757b00b7f18b855a8d813e689dc6ddef9
smilingfroggy <smilingfroggy@hotmail.com>
2024-02-07 18:12:10 +0800

    feat: GET /admin/reservations/:id

--- controllers/adminReserveController.js
+++ controllers/adminReserveController.js
@@ -34,7 +34,52 @@ const adminReserveController = {
       console.log(error)
       next(error)
     }
-  }
+  },
+  getReservation: async (req, res, next) => {
+    try {
+      const { reservationId } = req.params
+      if (!reservationId) throw new Error('ReservationId not exist')
+
+      // get reservation and associated collection, artworks, and user
+      let reservation = await Reservation.findByPk(reservationId, {
+        attributes: ['id', 'contact_person', 'phone', 'visitor_num', 'time', 'purpose', 'work_count', 'description', 'status', 'sn'],
+        include: [{ model: User, attributes: ['name', 'email'] },
+        {
+          model: Collection, attributes: ['id', 'name'], include:
+          {
+            model: Artwork,
+            as: 'JoinedArtworks',
+            attributes: ['id', 'name', 'artistName', 'serialNumber', 'creationTime', 'height', 'width', 'depth', 'piecesNum'],
+            through: { attributes: [] },
+            include: { model: Medium, attributes: ['name'] }
+          }
+        }
+        ]
+      })
+      if (!reservation) throw new Error('Reservation not exist')
+      reservation = reservation.toJSON()
+
+      // organize reservation's date, time, status; artworks' creationTime, size
+      reservation.date = dateHelpers.getDateString(reservation.time) // YYYY-MM-DD
+      reservation.timeSlot = dateHelpers.getTimeString(reservation.time) // HH:mm
+      if (!reservation.status) {
+        if (reservation.time < Date.now()) reservation.status = '逾期未付款'
+        else reservation.status = '待付款'
+      } else {
+        reservation.status = '預約完成'
+      }
+
+      reservation.Collection.JoinedArtworks.forEach(work => {
+        work.creationTime = work.creationTime ? new Date(work.creationTime).getFullYear() : ""
+        work.size = work.depth === "0" ? (work.height + "x" + work.width + "x" + work.depth) : (work.height + "x" + work.width)
+      })
+
+      return res.render('admin/reservation', { reservation })
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  },
 }
 
 
--- routes/pages/modules/admin.js
+++ routes/pages/modules/admin.js
@@ -44,6 +44,8 @@ router.delete('/artists/:artistId', authenticatedAdmin, adminArtistController.de
 router.delete('/artists/:artistId/images', authenticatedAdmin, adminArtistController.deleteArtistImages)
 
 router.get('/reservations', authenticatedAdmin, adminReserveController.getUpcomingReservations)
+router.get('/reservations/:reservationId', authenticatedAdmin, adminReserveController.getReservation)
+
 // test email
 // router.post('/testemail', authenticatedAdmin, emailController.testEmail)
 
